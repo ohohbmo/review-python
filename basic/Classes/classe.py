5fd6470f1d156a0511bb6d09bd2d87d9061d3216
ohohbmo <ohohbmo@proton.me>
2025-02-13 08:06:59 -0300

    backup cachyos kernel

--- basic/Classes/classe.py
+++ basic/Classes/classe.py
@@ -1,6 +1,10 @@
 class Task():
-    def __init__(self, nome='001'):
-        self.nome=nome
+    def __init__(self, id='001', tipo='base'):
+        self.id=id
+        self.tipo=tipo
+        self.dic_obj={
+            #'id':[relacao, local]
+        }
     def print_nome(self):
         print(self.nome)
 
@@ -17,3 +21,4 @@ tarefa_001.print_nome()
 tarefa_002 = TaskRecurrent('222')
 tarefa_002.print_nome()
 tarefa_002.loop()
+
